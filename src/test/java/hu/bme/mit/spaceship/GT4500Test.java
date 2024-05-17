029cc5ac688ec011df139e08e6bd2cee617d1377
cheshire0 <seboklili1@gmail.com>
2024-05-17 10:51:47 +0200

    izolacio

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,31 +9,40 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore primTorpedoStore;
+  private TorpedoStore secTorpedoStore;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    primTorpedoStore=mock(TorpedoStore.class);
+    secTorpedoStore=mock(TorpedoStore.class);
+    this.ship = new GT4500(primTorpedoStore,secTorpedoStore);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(primTorpedoStore.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
-
+    
     // Assert
+    verify(primTorpedoStore).fire(1);
     assertEquals(true, result);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+    when(primTorpedoStore.fire(1)).thenReturn(true);
+    when(secTorpedoStore.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
+    verify(primTorpedoStore).fire(1);
+    verify(secTorpedoStore).fire(1);
     assertEquals(true, result);
   }
 
